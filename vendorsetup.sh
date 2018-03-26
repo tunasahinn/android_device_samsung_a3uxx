fdba1a5a2c21df69ea997ffdb8f9bfa5a76b53e0
Jens Lody <jens@jenslody.de>
2018-03-26 02:02:42 +0200

    Initial commit for GZR-Validus.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 for i in eng userdebug user; do
-	add_lunch_combo lineage_j5nlte-${i}
+	add_lunch_combo validus_j5nlte-${i}
 done
+#add_lunch_combo validus_j5nlte-userdebug
+#add_lunch_combo validus_j5nlte-user
+
