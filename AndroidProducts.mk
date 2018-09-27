620c45842c9e79170f53ab345dc724d43be71547
Jens Lody <jens@jenslody.de>
2018-09-27 18:30:55 +0200

    Bootleggers fixes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,4 +3,5 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/omni_j5nlte.mk \
 	$(LOCAL_DIR)/rr_j5nlte.mk \
 	$(LOCAL_DIR)/pa_j5nlte.mk \
-	$(LOCAL_DIR)/lineage_j5nlte.mk
+	$(LOCAL_DIR)/lineage_j5nlte.mk \
+	$(LOCAL_DIR)/bootleg.mk
