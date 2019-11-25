4ad1528f240a23f5087678a68eada9a2c2e5631c
Jens Lody <jens@jenslody.de>
2019-11-25 20:16:35 +0100

    Nuke vendorsetup.sh and switch to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,9 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/rr_j5nlte.mk \
 	$(LOCAL_DIR)/pa_j5nlte.mk \
 	$(LOCAL_DIR)/lineage_j5nlte.mk \
-	$(LOCAL_DIR)/bootleg.mk
+	$(LOCAL_DIR)/bootleg_j5nlte.mk
+	
+COMMON_LUNCH_CHOICES := \
+    bootleg_j5nlte-eng \
+    bootleg_j5nlte-userdebug \
+    bootleg_j5nlte-user
