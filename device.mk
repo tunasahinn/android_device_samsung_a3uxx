47f73ffc0d09e4c0fa2eb768c6d8c3223ff51cc1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-23 11:22:43 +0100

    j5nlte: use msm8916-common

--- device.mk
+++ device.mk
@@ -2,7 +2,7 @@
 $(call inherit-product-if-exists, vendor/samsung/j5nlte/j5nlte-vendor.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/j5-common/device-common.mk)
+$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 LOCAL_PATH := device/samsung/j5nlte
 
