fe5225de97904ebae883138a0034e786e2992b31
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-23 11:22:26 +0100

    j5nlte: RevengeOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,8 @@
 LOCAL_PATH := device/samsung/j5nlte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/omni_j5nlte.mk \
-	$(LOCAL_DIR)/rr_j5nlte.mk \
-	$(LOCAL_DIR)/pa_j5nlte.mk \
-	$(LOCAL_DIR)/lineage_j5nlte.mk \
-	$(LOCAL_DIR)/bootleg_j5nlte.mk
+	$(LOCAL_DIR)/revengeos_j5nlte.mk \
 	
 COMMON_LUNCH_CHOICES := \
-    bootleg_j5nlte-eng \
-    bootleg_j5nlte-userdebug \
-    bootleg_j5nlte-user
+    revengeos_j5nlte-eng \
+    revengeos_j5nlte-userdebug \
+    revengeos_j5nlte-user
--- revengeos_j5nlte.mk
+++ revengeos_j5nlte.mk
@@ -1,12 +1,12 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/j5-common/rr.mk)
+$(call inherit-product, device/samsung/msm8916-common/revengeos.mk)
 
 $(call inherit-product, device/samsung/j5nlte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5nlte
-PRODUCT_NAME := rr_j5nlte
+PRODUCT_NAME := revengeos_j5nlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J500FN
 PRODUCT_MANUFACTURER := samsung
