785f8e86da1487f2c4f9c64d8229c077c83e9f33
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-26 09:12:42 +0100

    j5nlte: follow new common makefiles inheritting

--- device.mk
+++ device.mk
@@ -1,6 +1,12 @@
 #Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/j5nlte/j5nlte-vendor.mk)
 
+# Inherit J5 2015 device
+$(call inherit-product, device/samsung/msm8916-common/device-j5.mk)
+
+# Inherit NFC support
+$(call inherit-product, device/samsung/msm8916-common/nfc.mk)
+
 # Inherit from common
 $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
