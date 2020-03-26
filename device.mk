d19beaa8fcb3c63e2b8c048cd638ca99d04ed23b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-03-26 08:39:34 +0100

    j5nlte: commonize NFC
    Revert "j5nlte: build NFC HAL 1.1"
    
    This reverts commit 28aa173b644363079713ad1c600592f93236b2f8.
    
    Revert "j5nlte: update NFC permissions xmls"
    
    This reverts commit 1435369ece339fa6632a468a1ed95fe0b21bf31b.
    
    Revert "j5nlte: kang nfc configs from j4primelte"
    
    This reverts commit 5c8b05cd204f07c61a8b513e1e74f7c9ec59c990.

--- device.mk
+++ device.mk
@@ -11,31 +11,3 @@ LOCAL_PATH := device/samsung/j5nlte
 
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# NFC
-PRODUCT_PACKAGES += \
-	android.hardware.nfc@1.1 \
-	android.hardware.nfc@1.1-service \
-    vendor.nxp.nxpnfc@1.0 \
-    vendor.nxp.hardware.nfc@1.0-service
-
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    com.gsma.services.nfc \
-    com.nxp.nfc.nq \
-    nfc_nci.nqx.default \
-    nqnfcee_access.xml \
-    NfcNci \
-    Tag
-
-# NFC configs
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/libnfc-nxp.conf
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
