2bcdc1a0b61d5bd4274fc99dcd11b2d73acf5a0c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-04-21 02:03:18 +0200

    j5nlte: commonize props

--- device.mk
+++ device.mk
@@ -12,8 +12,5 @@ $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 LOCAL_PATH := device/samsung/j5nlte
 
-# System properties
--include $(LOCAL_PATH)/system_prop.mk
-
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
--- revengeos_j5nlte.mk
+++ revengeos_j5nlte.mk
@@ -7,9 +7,4 @@ $(call inherit-product, device/samsung/j5nlte/device.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5nlte
 PRODUCT_NAME := revengeos_j5nlte
-PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J500FN
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_CHARACTERISTICS := phone
-
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
